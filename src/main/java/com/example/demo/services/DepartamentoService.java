78420bb750883ed55981cda963e8f905f99f8508
Julian Lucano <julian97lucano@gmail.com>
2023-03-27 11:46:08 -0600

    update project

--- src/main/java/com/example/demo/services/DepartamentoService.java
+++ src/main/java/com/example/demo/services/DepartamentoService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entitys.Departamento;
@@ -10,11 +11,15 @@ import com.example.demo.repositories.DepartamentoRepository;
 
 @Service
 public class DepartamentoService {
+	
+	@Value("${spring.datasource.username}")
+	private String userName;
 
 	@Autowired
 	private DepartamentoRepository deptoRepository;
 	
 	public List<Departamento> getDepartamentos() {
+		System.out.println("USERNAME"+userName);
 		return deptoRepository.getDepartamentos();
 	}
 	
