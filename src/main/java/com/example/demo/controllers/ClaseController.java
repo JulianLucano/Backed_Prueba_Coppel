df16f6e1ed862d3f7d907f4259107d5c900bfa67
Julian Lucano <julian97lucano@gmail.com>
2023-03-27 15:16:23 -0600

    cambios

--- src/main/java/com/example/demo/controllers/ClaseController.java
+++ src/main/java/com/example/demo/controllers/ClaseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entitys.Clase;
 import com.example.demo.services.ClaseService;
 
+@CrossOrigin(origins= {"*"})
 @RestController
 @RequestMapping("/clases")
 public class ClaseController {
